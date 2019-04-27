0b1925522955eeaec326c839b6675fc9d27fe04f
Goran <gdraskovic@gmail.com>
2019-04-27 14:42:36 -0400

    Update googlesheets.js

--- assets/js/googlesheets.js
+++ assets/js/googlesheets.js
@@ -1,6 +1,6 @@
 var $form = $('form#contactForm'),
             url = 'https://script.google.com/macros/s/AKfycbyOHXy-cKd80hw1axokwNX3kcMbJnr3m-EUELz4Y6uazPw5K-Em/exec'
-        $('#submit-form').on('click', function(e) {
+        $('#form-submit').on('click', function(e) {
           e.preventDefault();
           var jqxhr = $.ajax({
             url: url,
